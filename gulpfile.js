240bbfbff9d059e07998f0d6f4653e35e374189d
devilesk <devilesk@gmail.com>
2016-08-23 18:50:31 -0400

    reorganize folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var spritesmith = require('gulp.spritesmith');
 
-gulp.task('miniheroes-fix', function() {
-    return gulp.src('./miniheroes/fix/*')
-        .pipe(gulp.dest('./miniheroes/src'));
+gulp.task('miniheroes-copy', function() {
+    return gulp.src('./images/miniheroes/src/*')
+        .pipe(gulp.dest('./dist/miniheroes'));
+});
+
+gulp.task('miniheroes-fix', ['miniheroes-copy'], function() {
+    return gulp.src('./images/miniheroes/fix/*')
+        .pipe(gulp.dest('./dist/miniheroes'));
 });
 
 gulp.task('miniheroes-sprite', ['miniheroes-fix'], function () {
-    var spriteData = gulp.src('./miniheroes/src/*').pipe(spritesmith({
+    var spriteData = gulp.src('./dist/miniheroes/*').pipe(spritesmith({
         imgName: 'miniheroes_sprite.png',
         cssName: 'miniheroes_sprite.css',
         cssVarMap: function (sprite) {
