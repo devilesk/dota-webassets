5a0bf8912158b820faa0ac4ea7ab413120e7051e
devilesk <devilesk@gmail.com>
2016-08-23 23:43:34 -0400

    update build process

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 dota-webassets
 ===============
 To be used at http://devilesk.com/dota2
+
+TODO: add spellicons and items
--- gulpfile.js
+++ gulpfile.js
@@ -3,146 +3,105 @@ var spritesmith = require('gulp.spritesmith');
 var imageResize = require('gulp-image-resize');
 var imagemin = require('gulp-imagemin');
 var gulpSequence = require('gulp-sequence');
+var merge = require('merge-stream');
+var buffer = require('vinyl-buffer');
+var csso = require('gulp-csso');
+var del = require('del');
 
-gulp.task('default', gulpSequence(['miniheroes', 'heroes', 'portraits'], 'image-min'));
+var imgRoot = '../images/';
 
-gulp.task('image-min', function() {
-    return gulp.src('./dist/**/*.png', { base: "./" })
-        .pipe(imagemin())
-        .pipe(gulp.dest('.'));
+function spriteTaskGenerator(imgRoot, name, sizePrefix) {
+    return function () {
+        var name_size = sizePrefix ?  name + '_' + sizePrefix : name;
+        var spriteData = gulp.src('./dist/images/' + name_size + '/*').pipe(spritesmith({
+            imgName: name_size + '_sprite.png',
+            imgPath: imgRoot + name + '/' + name_size + '_sprite.png',
+            cssName: name_size + '_sprite.css',
+            cssOpts: {
+                cssSelector: function (sprite) {
+                    return '.' + name + '-sprite-' + sprite.name + (sizePrefix ? ' .' + name + '-sprite-' + sizePrefix : '');
+                }
+            }
+        }));
+        
+        var imgStream = spriteData.img
+            .pipe(buffer())
+            .pipe(imagemin())
+            .pipe(gulp.dest('./dist/images/' + name_size));
+
+        var cssStream = spriteData.css
+            .pipe(csso())
+            .pipe(gulp.dest('./dist/css'));
+
+        return merge(imgStream, cssStream);
+    }
+}
+
+function taskGenerator(imgRoot, name, sizes) {
+    gulp.task(name + '-clean', function () {
+        return del([
+            './dist/images/' + name + '/*',
+            './dist/css/' + name + '_sprite.css',
+        ], {force: true});
+    });
+
+    gulp.task(name + '-copy', [name + '-clean'], function() {
+        return gulp.src('./images/' + name + '/src/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./dist/images/' + name));
+    });
+    
+    gulp.task(name + '-fix', [name + '-copy'], function() {
+        return gulp.src('./images/' + name + '/fix/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./dist/images/' + name));
+    });
+    
+    gulp.task(name + '-sprite', [name + '-fix'], spriteTaskGenerator(imgRoot, name));
+    
+    var spriteTasks = [name + '-sprite'];
+    
+    var sizes = sizes || [];
+    sizes.forEach(function (size) {
+        var width = size[0];
+        var height = size[1];
+        var sizePrefix = width + 'x' + height;
+    
+        gulp.task(name + '-clean-' + sizePrefix, function () {
+            return del([
+                './dist/images/' + name + '_' + sizePrefix + '/*',
+                './dist/css/' + name + '_sprite' + '_' + sizePrefix + '.css',
+            ], {force: true});
+        });
+
+        gulp.task(name + '-resize-' + sizePrefix, [name + '-fix', name + '-clean-' + sizePrefix], function() {
+            return gulp.src('./dist/images/' + name + '/*')
+                .pipe(imageResize({
+                    width : width,
+                    height : height,
+                    crop : true,
+                    upscale : false
+                }))
+                .pipe(imagemin())
+                .pipe(gulp.dest('./dist/images/' + name + '_' + sizePrefix));
+        });
+        
+        gulp.task(name + '-sprite-' + sizePrefix, [name + '-resize-' + sizePrefix], spriteTaskGenerator(imgRoot, name, sizePrefix));
+        
+        spriteTasks.push(name + '-sprite-' + sizePrefix);
+    });
+
+    gulp.task(name, spriteTasks);
+}
+
+taskGenerator(imgRoot, 'miniheroes');
+taskGenerator(imgRoot, 'heroes', [[64, 36], [32, 18]]);
+taskGenerator(imgRoot, 'portraits', [[64, 84], [32, 42]]);
+    
+gulp.task('clean', function () {
+    return del([
+        './dist/**/*'
+    ], {force: true});
 });
-
-
-/* MINIHEROES */
-gulp.task('miniheroes', ['miniheroes-sprite']);
-
-gulp.task('miniheroes-copy', function() {
-    return gulp.src('./images/miniheroes/src/*')
-        .pipe(gulp.dest('./dist/miniheroes'));
-});
-
-gulp.task('miniheroes-fix', ['miniheroes-copy'], function() {
-    return gulp.src('./images/miniheroes/fix/*')
-        .pipe(gulp.dest('./dist/miniheroes'));
-});
-
-gulp.task('miniheroes-sprite', ['miniheroes-fix'], function () {
-    var spriteData = gulp.src('./dist/miniheroes/*').pipe(spritesmith({
-        imgName: 'miniheroes_sprite.png',
-        cssName: 'miniheroes_sprite.css',
-        cssVarMap: function (sprite) {
-          sprite.name = 'miniheroes_' + sprite.name;
-        }
-    }));
-    return spriteData.pipe(gulp.dest('dist'));
-});
-
-
-/* HEROES */
-gulp.task('heroes', ['heroes-sprite-64x36', 'heroes-sprite-32x18']);
-
-gulp.task('heroes-copy', function() {
-    return gulp.src('./images/heroes/src/*')
-        .pipe(gulp.dest('./dist/heroes'));
-});
-
-gulp.task('heroes-resize-64x36', ['heroes-copy'], function() {
-    return gulp.src('./dist/heroes/*')
-        .pipe(imageResize({
-            width : 64,
-            height : 36,
-            crop : true,
-            upscale : false
-        }))
-        .pipe(gulp.dest('./dist/heroes_64x36'));
-});
-
-gulp.task('heroes-resize-32x18', ['heroes-copy'], function() {
-    return gulp.src('./dist/heroes/*')
-        .pipe(imageResize({
-            width : 32,
-            height : 18,
-            crop : true,
-            upscale : false
-        }))
-        .pipe(gulp.dest('./dist/heroes_32x18'));
-});
-
-gulp.task('heroes-sprite-64x36', ['heroes-resize-64x36'], function () {
-    var spriteData = gulp.src('./dist/heroes_64x36/*').pipe(spritesmith({
-        imgName: 'heroes_sprite_64x36.png',
-        cssName: 'heroes_sprite_64x36.css',
-        cssVarMap: function (sprite) {
-          sprite.name = 'heroes64x36_' + sprite.name;
-        }
-    }));
-    return spriteData.pipe(gulp.dest('dist'));
-});
-
-gulp.task('heroes-sprite-32x18', ['heroes-resize-32x18'], function () {
-    var spriteData = gulp.src('./dist/heroes_32x18/*').pipe(spritesmith({
-        imgName: 'heroes_sprite_32x18.png',
-        cssName: 'heroes_sprite_32x18.css',
-        cssVarMap: function (sprite) {
-          sprite.name = 'heroes32x18_' + sprite.name;
-        }
-    }));
-    return spriteData.pipe(gulp.dest('dist'));
-});
-
-/* PORTRAITS */
-gulp.task('portraits', ['portraits-sprite-64x84', 'portraits-sprite-32x42']);
-
-gulp.task('portraits-copy', function() {
-    return gulp.src('./images/portraits/src/*')
-        .pipe(gulp.dest('./dist/portraits'));
-});
-
-gulp.task('portraits-fix', ['portraits-copy'], function() {
-    return gulp.src('./images/portraits/fix/*')
-        .pipe(gulp.dest('./dist/portraits'));
-});
-
-gulp.task('portraits-resize-64x84', ['portraits-fix'], function() {
-    return gulp.src('./dist/portraits/*')
-        .pipe(imageResize({
-            width : 64,
-            height : 84,
-            crop : true,
-            upscale : false
-        }))
-        .pipe(gulp.dest('./dist/portraits_64x84'));
-});
-
-gulp.task('portraits-resize-32x42', ['portraits-fix'], function() {
-    return gulp.src('./dist/portraits/*')
-        .pipe(imageResize({
-            width : 32,
-            height : 42,
-            crop : true,
-            upscale : false
-        }))
-        .pipe(gulp.dest('./dist/portraits_32x42'));
-});
-
-gulp.task('portraits-sprite-64x84', ['portraits-resize-64x84'], function () {
-    var spriteData = gulp.src('./dist/portraits_64x84/*').pipe(spritesmith({
-        imgName: 'portraits_sprite_64x84.png',
-        cssName: 'portraits_sprite_64x84.css',
-        cssVarMap: function (sprite) {
-          sprite.name = 'portraits64x84_' + sprite.name;
-        }
-    }));
-    return spriteData.pipe(gulp.dest('dist'));
-});
-
-gulp.task('portraits-sprite-32x42', ['portraits-resize-32x42'], function () {
-    var spriteData = gulp.src('./dist/portraits_32x42/*').pipe(spritesmith({
-        imgName: 'portraits_sprite_32x42.png',
-        cssName: 'portraits_sprite_32x42.css',
-        cssVarMap: function (sprite) {
-          sprite.name = 'portraits32x42_' + sprite.name;
-        }
-    }));
-    return spriteData.pipe(gulp.dest('dist'));
-});
+    
+gulp.task('default', ['miniheroes', 'heroes', 'portraits']);
