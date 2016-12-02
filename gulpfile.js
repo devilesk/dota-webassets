d83d3e53117a14ea98d3a15f2e99d12218672326
devilesk <devilesk@gmail.com>
2016-12-02 12:39:03 -0500

    add jpg spritesheets

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,13 @@ var del = require('del');
 
 var imgRoot = '../images/';
 
-function spriteTaskGenerator(imgRoot, name, sizePrefix) {
+function spriteTaskGenerator(imgRoot, name, sizePrefix, imageExt) {
     return function () {
         var name_size = sizePrefix ?  name + '_' + sizePrefix : name;
         var spriteData = gulp.src('./dist/images/' + name_size + '/*').pipe(spritesmith({
-            imgName: name_size + '_sprite.png',
-            imgPath: imgRoot + name_size + '/' + name_size + '_sprite.png',
-            cssName: name_size + '_sprite.css',
+            imgName: name_size + '_sprite.' + imageExt,
+            imgPath: imgRoot + name_size + '/' + name_size + '_sprite.' + imageExt,
+            cssName: name_size + '_sprite_' + imageExt + '.css',
             cssOpts: {
                 cssSelector: function (sprite) {
                     return '.' + name + '-sprite-' + sprite.name.replace('.', '-') + (sizePrefix ? '.' + name + '-sprite-' + sizePrefix : '');
@@ -37,44 +37,44 @@ function spriteTaskGenerator(imgRoot, name, sizePrefix) {
     }
 }
 
-function taskGenerator(imgRoot, name, sizes) {
-    gulp.task(name + '-clean', function () {
-        return del([
-            './dist/images/' + name + '/*',
-            './dist/css/' + name + '_sprite.css',
-        ], {force: true});
+function taskGenerator(imgRoot, name, sizes, imgTypes) {
+    var cleanTaskName = name + '-clean';
+    var copyTaskName = name + '-copy';
+    var fixTaskName = name + '-fix';
+    
+    gulp.task(cleanTaskName, function () {
+        var arr = imgTypes.map(function (imgType) { return './dist/css/' + name + '_sprite_' + imgType + '.css'; });
+        arr.push('./dist/images/' + name + '/*');
+        return del(arr, {force: true});
     });
-
-    gulp.task(name + '-copy', [name + '-clean'], function() {
+    
+    gulp.task(copyTaskName, function() {
         return gulp.src('./images/' + name + '/src/*')
             .pipe(imagemin())
             .pipe(gulp.dest('./dist/images/' + name));
     });
     
-    gulp.task(name + '-fix', [name + '-copy'], function() {
+    gulp.task(fixTaskName, function() {
         return gulp.src('./images/' + name + '/fix/*')
             .pipe(imagemin())
             .pipe(gulp.dest('./dist/images/' + name));
     });
-    
-    gulp.task(name + '-sprite', [name + '-fix'], spriteTaskGenerator(imgRoot, name));
-    
-    var spriteTasks = [name + '-sprite'];
-    
+        
     var sizes = sizes || [];
     sizes.forEach(function (size) {
         var width = size[0];
         var height = size[1];
         var sizePrefix = width + 'x' + height;
     
-        gulp.task(name + '-clean-' + sizePrefix, function () {
-            return del([
-                './dist/images/' + name + '_' + sizePrefix + '/*',
-                './dist/css/' + name + '_sprite' + '_' + sizePrefix + '.css',
-            ], {force: true});
+        var cleanTaskName2 = name + '-clean-' + sizePrefix;
+        
+        gulp.task(cleanTaskName2, function () {
+            var arr = imgTypes.map(function (imgType) { return './dist/css/' + name + '_sprite' + '_' + sizePrefix + '_' + imgType + '.css'; });
+            arr.push('./dist/images/' + name + '_' + sizePrefix + '/*');
+            return del(arr, {force: true});
         });
 
-        gulp.task(name + '-resize-' + sizePrefix, [name + '-fix', name + '-clean-' + sizePrefix], function() {
+        gulp.task(name + '-resize-' + sizePrefix, function() {
             return gulp.src('./dist/images/' + name + '/*')
                 .pipe(imageResize({
                     width : width,
@@ -85,20 +85,54 @@ function taskGenerator(imgRoot, name, sizes) {
                 .pipe(imagemin())
                 .pipe(gulp.dest('./dist/images/' + name + '_' + sizePrefix));
         });
-        
-        gulp.task(name + '-sprite-' + sizePrefix, [name + '-resize-' + sizePrefix], spriteTaskGenerator(imgRoot, name, sizePrefix));
-        
-        spriteTasks.push(name + '-sprite-' + sizePrefix);
     });
+        
+    imgTypes.forEach(function (imgType) {
+        var taskName = name + '-sprite-' + imgType;
+        
+        gulp.task(taskName, spriteTaskGenerator(imgRoot, name, null, imgType));
+        
+        var spriteTasks = [taskName];
+        
+        sizes.forEach(function (size) {
+            var width = size[0];
+            var height = size[1];
+            var sizePrefix = width + 'x' + height;
+            
+            var taskName2 = name + '-sprite-' + sizePrefix + '-' + imgType;
+            
+            gulp.task(taskName2, spriteTaskGenerator(imgRoot, name, sizePrefix, imgType));
+            
+            spriteTasks.push(taskName2);
+        });
 
-    gulp.task(name, spriteTasks);
+        gulp.task(name + '-' + imgType, gulpSequence.apply(this, spriteTasks));
+    });
+    
+    var taskList = [cleanTaskName, copyTaskName, fixTaskName]
+    imgTypes.forEach(function (imgType) {
+        taskList.push(name + '-sprite-' + imgType);
+    });
+    sizes.forEach(function (size) {
+        var width = size[0];
+        var height = size[1];
+        var sizePrefix = width + 'x' + height;
+    
+        taskList.push(name + '-clean-' + sizePrefix);
+        taskList.push(name + '-resize-' + sizePrefix);
+            
+        imgTypes.forEach(function (imgType) {
+            taskList.push(name + '-sprite-' + sizePrefix + '-' + imgType)
+        });
+    });
+    gulp.task(name, gulpSequence.apply(this, taskList));
 }
 
-taskGenerator(imgRoot, 'miniheroes');
-taskGenerator(imgRoot, 'heroes', [[64, 36], [32, 18]]);
-taskGenerator(imgRoot, 'portraits', [[64, 84], [32, 42]]);
-taskGenerator(imgRoot, 'items', [[50, 36], [36, 26], [25, 18]]);
-taskGenerator(imgRoot, 'spellicons', [[64, 64], [32, 32]]);
+taskGenerator(imgRoot, 'miniheroes', null, ['png', 'jpg']);
+taskGenerator(imgRoot, 'heroes', [[64, 36], [32, 18]], ['png', 'jpg']);
+taskGenerator(imgRoot, 'portraits', [[64, 84], [32, 42]], ['png', 'jpg']);
+taskGenerator(imgRoot, 'items', [[70, 50], [50, 36], [36, 26], [25, 18]], ['png', 'jpg']);
+taskGenerator(imgRoot, 'spellicons', [[64, 64], [32, 32]], ['png', 'jpg']);
     
 gulp.task('clean', function () {
     return del([
@@ -106,4 +140,4 @@ gulp.task('clean', function () {
     ], {force: true});
 });
     
-gulp.task('default', gulpSequence('clean', ['miniheroes', 'heroes', 'portraits', 'items', 'spellicons']));
+gulp.task('default', gulpSequence('clean', 'spellicons', 'items', 'heroes', 'portraits', 'miniheroes'));
